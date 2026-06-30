25f14a08994579941c537b32f766ba40426aa0d8
tom1299 <tom1299@jeeatwork.com>
2026-06-30 14:16:00 +0200

    Change method for git diff retrieval add llm analysis test

--- tests/unit/git/test_git.py
+++ tests/unit/git/test_git.py
@@ -2,8 +2,11 @@ import logging
 import os
 import sys
 from typing import NamedTuple, Union, Optional
+from unittest import skip
 
 from pytest import fixture, mark
+
+from lctutorial import init_chat_model
 
 from git import Repo
 
@@ -22,39 +25,30 @@ class DiffDetails(NamedTuple):
     diff: str
     commit_message: Union[str, bytes, None]
     new_version: str
+    prev_version: str
+    prev_commit_sha: str
 
-def get_diff_details(commit_hash: str, repo: Repo, file: str) -> Optional[DiffDetails]:
-    commit = repo.commit(commit_hash)
-    parent_commit = commit.parents[0] if commit.parents else None
+def get_diff_details(commit_sha: str, repo: Repo, file_path: str) -> Optional[DiffDetails]:
+    commit = repo.commit(commit_sha)
 
-    if parent_commit:
-        diffs = commit.diff(parent_commit, paths=os.path.join(repo.working_dir, file), create_patch=True)
-        for diff in diffs:
-            new_version = commit.tree[file].data_stream.read().decode("utf-8")
-            return DiffDetails(diff=diff.diff.decode("utf-8"), commit_message=commit.message, new_version=new_version)
+    commits = list(repo.iter_commits(paths=file_path))
+
+    # TODO: Refactor / improve code
+    for i, c in enumerate(commits):
+        if c.hexsha == commit_sha:
+            if i + 1 < len(commits):
+                parent_commit = commits[i + 1]
+                previous_version = parent_commit.tree[file_path].data_stream.read().decode("utf-8")
+                diffs = commit.diff(parent_commit, paths=os.path.join(repo.working_dir, file_path), create_patch=True)
+                for diff in diffs:
+                    new_version = commit.tree[file_path].data_stream.read().decode("utf-8")
+                    return DiffDetails(diff=diff.diff.decode("utf-8"), commit_message=commit.message,
+                                       new_version=new_version, prev_version=previous_version, prev_commit_sha=parent_commit.hexsha)
 
     return None
 
 @mark.parametrize("path_to_git_repo", ["kubernetes_documentation"])
 class TestGitApi:
-
-    def test_get_diff_for_file_and_commits(self, path_to_git_repo, request):
-        logging.getLogger("test").info("Start test")
-        repo = Repo(request.getfixturevalue(path_to_git_repo))
-        service_md = os.path.join(repo.working_dir, "content/en/docs/concepts/services-networking/service.md")
-        commits = repo.iter_commits(all=True, max_count=10, paths=service_md)
-        prev_commit = None
-        for commit in commits:
-            if prev_commit:
-                diffs = commit.diff(other=prev_commit, paths=service_md, create_patch=True)
-                for diff in diffs:
-                    print(commit.message, diff.diff)
-                new_version = commit.tree["content/en/docs/concepts/services-networking/service.md"]
-
-                # previous_document = prev_commit.tree["content/en/docs/concepts/services-networking/service.md"]
-                # print(previous_document.data_stream.read().decode("utf-8"))
-
-            prev_commit = commit
 
     def test_get_diff_details(self, path_to_git_repo, request):
         repo = Repo(request.getfixturevalue(path_to_git_repo))
@@ -62,5 +56,45 @@ class TestGitApi:
         diff_details = get_diff_details("4a1f03802b423637c04c56258f87630a28139217", repo, service_md)
 
         assert diff_details.diff is not None
+        assert diff_details.prev_commit_sha == '3d3d53bb471d8b0f145ae98e3fa95e34bfc2d113'
         assert diff_details.commit_message is not None
         assert "Consider using an external load balancer controller or a Gateway API implementation instead" in diff_details.new_version
+        assert "Consider using an external load balancer controller or a Gateway API implementation instead" not in diff_details.prev_version
+
+@mark.parametrize("path_to_git_repo", ["kubernetes_documentation"])
+@mark.parametrize(
+    ("provider", "model"),
+    [
+        ("OpenAI", "gpt-4.1-mini"),
+#        ("anthropic", "claude-3-5-sonnet"),
+    ],
+    ids=["gpt-4.1-mini"]#, "anthropic-sonnet"],
+)
+@skip("Skipping test_get_diff_details_with_model due to API key issues")
+class TestAgentGitAnalysis:
+    pass
+
+    def test_get_diff_details_with_model(self, path_to_git_repo, request, provider, model):
+        repo = Repo(request.getfixturevalue(path_to_git_repo))
+        service_md = "content/en/docs/concepts/services-networking/service.md"
+        diff_details = get_diff_details("3d3d53bb471d8b0f145ae98e3fa95e34bfc2d113", repo, service_md)
+
+        model = init_chat_model(provider=provider, model_name=model)
+
+        prompt = f"""
+        You are a helpful assistant that analyzes git diffs for documentation changes.
+        You get the git diff and the version of the document before the change.
+        Your task is to evaluate the changes made in a git commit.
+        * Topic of change
+        * Impact of the change to the overall content of the document 
+        * Impact of the change to the readability of the document
+        =========
+        Git diff:
+        {diff_details.diff}
+        ========
+        Previous version:
+        {diff_details.prev_version}
+        ========
+        """
+        response = model.invoke(prompt)
+        print(response)
